050f077bdc3221286d3bbdd5ee0f9a94100e5163
katepolozyuk <48688405+katepolozyuk@users.noreply.github.com>
2019-03-26 12:11:02 +0300

    hhh

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var csso = require('gulp-csso'); 
+var rename = require("gulp-rename");
+var scss = require('gulp-sass');
+
+gulp.task('minCss', function () {
+    return gulp.src('./app/css/style.css')    
+       .pipe(csso()) 
+       .pipe(rename({ suffix: ".min" })) 
+       .pipe(gulp.dest('./dist/css')); 
+});
+
+gulp.task('scssTest', function(){
+    return gulp.src('./app/sсss/*.scss') 
+    .pipe(scss())
+    .pipe(gulp.dest('./app/css'));
+});
+ //до запуска вотс базовое обновление файла
+gulp.task('watch', gulp.series (['scssTest'],['minCss'], function(){
+    gulp.watch('./app/sсss/*.scss',gulp.series(['scssTest'], ['minCss']));
+}));
